b15713a4d90103916133dfa617bdffc8fec2fcd7
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-03-14 22:45:27 +0300

    примерные две функции, регистр и вход

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template, request
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+mysql = MySQL(app)
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # Логика регистрации пользователя
-        return 'Пользователь зарегистрирован'
-    return render_template('register.html')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username and password:
+            return login()
+    return render_template('register_form.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username and password:
+            return f'Welcome, {username}'
+        else:
+            return 'ERORR BLEAD'
+    return render_template('confirm_form.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
